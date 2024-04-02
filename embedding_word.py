d3b4d18abf50e0447069f57b62f56090096b64f0
kimhonggithub <kimhong.set@student.cadt.edu.kh>
2024-04-02 14:00:58 +0700

    text_to_word_embeddings

--- embedding_word.py
+++ embedding_word.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 # Convert text data using Word2Vec embeddings
 def text_to_word_embeddings(text_data, word2vec_model):
     # Initialize an empty list to store the word embeddings for each review
@@ -8,13 +7,13 @@ def text_to_word_embeddings(text_data, word2vec_model):
     for review in text_data:
         # Tokenize the review into individual words
         words = review.split()
+        # Filter out words not in the Word2Vec vocabulary
+        words = [word for word in words if word in word2vec_model]
         # Get the embeddings for each word and take the mean to get the review's embedding
-        word_vectors = [word2vec_model.wv[word] for word in words if word in word2vec_model.wv]
-        if word_vectors:
-            embeddings.append(np.mean(word_vectors, axis=0))
+        if words:
+            embeddings.append(np.mean(word2vec_model[words], axis=0))
         else:
             # If no words in the vocabulary, return a vector of zeros
             embeddings.append(np.zeros(word2vec_model.vector_size))
             
     return np.array(embeddings)
-
